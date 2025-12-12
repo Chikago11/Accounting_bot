3a314571d00fbd5c5bce380d83327c5de6c0cb75
Andrei <99053272+Chikago11@users.noreply.github.com>
2025-12-12 17:36:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from datetime import datetime
 from google_api import add_expense_matrix, get_month_totals
 from config import TOKEN
 
+# edited on GitHub for test pull git
+
 CATEGORIES = [
     "Дом, телефон, интернет",
     "Супермаркет",
